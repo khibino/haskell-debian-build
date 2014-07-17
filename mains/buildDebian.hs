2777f9b326b9021256d1c82f2be61c433deaab0b
Kei Hibino <ex8k.hibino@gmail.com>
2014-07-17 15:38:04 +0900

    Update on-the-fly debian build command.

--- mains/buildDebian.hs
+++ mains/buildDebian.hs
@@ -2,22 +2,17 @@ import System.Environment (getArgs)
 
 import Debian.Package.Command (BuildMode (All))
 import qualified Debian.Package.Command as Command
-import Debian.Package.Source
 import Debian.Package.Build
-  (baseDirCurrent, defaultConfig,
-   Build, runBuild, runIO, withCurrentDir, removeBuildDir, rsyncGenSources)
-
-setupSrcDir :: Build FilePath
-setupSrcDir =  do
-  p            <- runIO $ parsePackageFromChangeLog "debian/changelog"
-  (_, srcDir)  <- rsyncGenSources p
-  return srcDir
+  (baseDirCurrent, defaultConfig, Build, runBuild, runIO,
+   withCurrentDir, removeBuildDir, genSources)
 
 build :: [String] -> Build ()
 build opts = do
   removeBuildDir
-  dir <- setupSrcDir
-  withCurrentDir dir . runIO $ Command.build All opts
+  mayS <- genSources
+  case mayS of
+    Just (_, dir) -> withCurrentDir dir . runIO $ Command.build All opts
+    Nothing       -> fail "Illegal state: genSources"
 
 main :: IO ()
 main =  do
