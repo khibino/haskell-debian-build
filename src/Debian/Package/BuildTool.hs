379fd3fc0b8383f5d1f3743e28350b3561405711
Kei Hibino <ex8k.hibino@gmail.com>
2014-07-23 02:56:55 +0900

    Extend integrated BuildTool module.

--- src/Debian/Package/BuildTool.hs
+++ src/Debian/Package/BuildTool.hs
@@ -1,11 +1,15 @@
 module Debian.Package.BuildTool
-       ( module Debian.Package.Monad
+       ( module Debian.Package.Hackage
+       , module Debian.Package.Monad
+       , module Debian.Package.Source
        , module Debian.Package.Command
        , module Debian.Package.Cabal
        , module Debian.Package.Build
        ) where
 
+import Debian.Package.Hackage
 import Debian.Package.Monad
+import Debian.Package.Source
 import Debian.Package.Command
-import Debian.Package.Cabal
+import Debian.Package.Cabal hiding (hackageLongName, hackageName, hackageVersion)
 import Debian.Package.Build
