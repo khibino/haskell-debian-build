eecb9174bd763db07758ee29435bab37977fbade
Kei Hibino <ex8k.hibino@gmail.com>
2014-07-31 16:39:43 +0900

    Add data-type namespace module.

--- src/Debian/Package/Build.hs
+++ src/Debian/Package/Build.hs
@@ -1,14 +1,10 @@
 module Debian.Package.Build
-       ( module Debian.Package.Data.Hackage
-       , module Debian.Package.Data.Source
-       , module Debian.Package.Build.Monad
+       ( module Debian.Package.Build.Monad
        , module Debian.Package.Build.Command
        , module Debian.Package.Build.Cabal
        , module Debian.Package.Build.Sequence
        ) where
 
-import Debian.Package.Data.Hackage
-import Debian.Package.Data.Source
 import Debian.Package.Build.Monad
 import Debian.Package.Build.Command
 import Debian.Package.Build.Cabal hiding (hackageLongName, hackageName, hackageVersion)
---
+++ src/Debian/Package/Data.hs
@@ -0,0 +1,7 @@
+module Debian.Package.Data
+       ( module Debian.Package.Data.Hackage
+       , module Debian.Package.Data.Source
+       ) where
+
+import Debian.Package.Data.Hackage
+import Debian.Package.Data.Source
